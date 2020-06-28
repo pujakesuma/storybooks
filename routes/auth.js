84a673d023c25297edb0afafac84b774c2773b62
ahmad ridwan <ahmadridwan.3107@gmail.com>
2020-06-28 15:54:48 +0700

    middleware

--- routes/auth.js
+++ routes/auth.js
@@ -14,4 +14,11 @@ router.get('/google/callback', passport.authenticate('google', {
   res.redirect('/dashboard')
 })
 
+// @desc  Logout user
+//@route  GET /auth/logout
+router.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/')
+})
+
 module.exports = router
